6d302315f5e2c11c13932da354b536f008004312
RoiuGit <puzikov.nikita@gmail.com>
2023-03-24 15:09:59 +0200

    changed formula evaluation logic and toString() method

--- src/Formula.java
+++ src/Formula.java
@@ -5,28 +5,17 @@ public class Formula {
     private String sign;
     private Formula antecedent;
     private Formula consequent;
-    private int signIndex;
     private boolean isAtomicFormula;
 
     Formula(String expression) {
         this.expression = normalize(expression);
-        sign = "";
-        evaluate(this.expression);
-        if (Objects.equals(sign, "") || Objects.equals(sign, "~")) {
-            isAtomicFormula = true;
-        }
-        if (Objects.equals(sign, "")) {
-            signIndex = 0;
-            antecedent = this;
-        }
-        if (Objects.equals(sign, "~")) {
-            signIndex = 0;
-            antecedent = new Formula(expression.substring(1));
-        }
+        evaluate();
     }
 
     private static String normalize(String expression) {
-        expression = expression.toUpperCase().replaceAll("\\s+","");
+        expression = expression.toUpperCase()
+                .replaceAll("\\s+", "")
+                .replace('V', 'v');
         expression = removeExcessParentheses(expression);
         return expression;
     }
@@ -36,44 +25,69 @@ public class Formula {
         return expression;
     }
 
-    private void evaluate(String expression) {
-        int openParens = -1;
-        for (int i = 0; i < expression.length(); i++) {
+    private boolean isValid() {
+        boolean containsValidCharacters;
+        String expression = this.expression.toUpperCase();
+        if (!isAtomicFormula) containsValidCharacters = expression.matches("^\\((([A-Z0-9()~&]|->)+)(->|V|&)" +
+                "(([A-Z0-9()~&]|->)+)\\)$");
+        else containsValidCharacters = expression.matches("^([A-Z&~()]|->)+$");
+        return containsValidCharacters;
+    }
 
+    private boolean isBalanced() {
+        int openParens = 0;
+        for (int i = 0; i < expression.length(); i++) {
             switch (expression.charAt(i)) {
                 case '(' -> openParens++;
                 case ')' -> openParens--;
-                case '>' -> {
-                    if (openParens == 0 && Objects.equals(sign, "")) {
-                        sign = "->";
-                        signIndex = i;
-                        antecedent = new Formula(expression.substring(1, signIndex - 1));
-                        consequent = new Formula(expression.substring(signIndex + 1, expression.length() - 1));
+            }
+        }
+        return openParens == 0;
+    }
+
+    private void evaluate() {
+        if (!isBalanced()) throw new IllegalArgumentException();
+        int openParens = -1;
+        boolean foundSign = false;
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+            int signIndex;
+            if (i == 0 && currentChar == '~') {
+                sign = "~";
+                signIndex = i;
+                isAtomicFormula = true;
+                antecedent = new Formula(expression.substring(signIndex + 1));
+                foundSign = true;
+                break;
+            }
+            switch (currentChar) {
+                case '(' -> openParens++;
+                case ')' -> openParens--;
+                case '-' -> {
+                    if (openParens == 0 && i + 1 < expression.length() && expression.charAt(i + 1) == '>') {
+                        foundSign = true;
+                        i++;
                     }
                 }
-                case '&' -> {
-                    if (openParens == 0 && Objects.equals(sign, "")) {
-                        sign = "&";
-                        signIndex = i;
-                        antecedent = new Formula(expression.substring(1, signIndex));
-                        consequent = new Formula(expression.substring(signIndex + 1, expression.length() - 1));
-                    }
-                }
-                case 'V' -> {
-                    if (openParens == 0 && Objects.equals(sign, "")) {
-                        sign = "v";
-                        signIndex = i;
-                        antecedent = new Formula(expression.substring(1, signIndex));
-                        consequent = new Formula(expression.substring(signIndex + 1, expression.length() - 1));
-                    }
-                }
-                case '~' -> {
-                    if (openParens < 1 && Objects.equals(sign, "")) {
-                        sign = "~";
+                case 'v', '&' -> {
+                    if (openParens == 0) {
+                        foundSign = true;
                     }
                 }
             }
+            if (foundSign) {
+                signIndex = i;
+                sign = currentChar == '-' ? "->" : String.valueOf(currentChar);
+                antecedent = new Formula(expression.substring(1, signIndex - (sign.equals("->") ? 1 : 0)));
+                consequent = new Formula(expression.substring(signIndex + 1, expression.length() - 1));
+                break;
+            }
         }
+        if (!foundSign) {
+            isAtomicFormula = true;
+            antecedent = this;
+        }
+        if (!isValid()) throw new IllegalArgumentException();
     }
 
     public String getSign() {
@@ -108,7 +122,6 @@ public class Formula {
 
     @Override
     public String toString() {
-        if (isAtomicFormula) return expression;
-        else return "(" + antecedent + sign + consequent + ")";
+        return expression;
     }
 }
